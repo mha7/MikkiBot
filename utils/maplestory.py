3601d15a652e192c68c5c81b42bf6ac61ccbfbbb
mha7 <mha7@users.noreply.github.com>
2017-10-25 14:03:39 -0700

    Fix empty return when searching a maple char
    Also added additional information when returning character info.

--- utils/maplestory.py
+++ utils/maplestory.py
@@ -2,6 +2,7 @@ import re
 import bs4
 import json
 import discord
+import requests
 import urllib.request
 from discord.ext import commands
 from collections import OrderedDict
@@ -12,6 +13,10 @@ class Maplestory:
     # ========================================= INITIALIZATION =========================================
     def __init__(self, bot):
         self.bot = bot
+        self.explorer = ['beginner', 'warrior', 'magician', 'bowman', 'thief', 'pirate']
+        self.cygnus = ['noblesse', 'dawn warrior', 'blaze wizard', 'wind archer', 'night walker', 'thunder breaker']
+        self.resistance = ['citizen', 'demon slayer', 'battle mage', 'wild hunter', 'mechanic']
+        self.sengoku = ['hayato', 'kanna']
 
     # ============================================ COMMANDS ============================================
 
@@ -22,11 +27,11 @@ class Maplestory:
             return
 
         info = self._search_player(player_name)
-        if not player_name: # No player found
-            await self.bot.say('Unable to find the player: ' + player_name)
-        else:
-            em = EmbedBuilder.build(info)
-            await self.bot.send_message(ctx.message.channel, embed=em)
+        if not info: # No player found
+            info['title'] = 'No player found!'
+
+        em = EmbedBuilder.build(info)
+        await self.bot.send_message(ctx.message.channel, embed=em)
 
     # ======================================== COMMAND HANDLERS ========================================
 
@@ -35,14 +40,23 @@ class Maplestory:
         url = "http://maplestory.nexon.net/rankings/overall-ranking/legendary?" \
               "pageIndex=1&character_name="+player_name+"&search=true#ranking"
 
-        source = urllib.request.urlopen(url).read()
+        source = requests.get(url)
 
-        soup = bs4.BeautifulSoup(source, 'html.parser')
+        soup = bs4.BeautifulSoup(source.text, 'html.parser')
 
         info = dict()
-        for tr in soup.find_all('tr'):
+        extra = None
 
-            if player_name in tr.text.lower():
+        ign = None
+        ranking = None
+        thumbnail = None
+        server = None
+        job = None
+
+        found = False
+
+        for tr in soup.find_all('tr'):
+            if player_name.lower() in tr.text.lower():
                 td = tr.find_all('td')
 
                 ranking = str(td[0].string).strip()               # Ranking
@@ -55,29 +69,112 @@ class Maplestory:
                 level_string = level[0] + '(' + level[1] + ')'
 
                 # Setting up general info
-                info['title'] = 'Maplestory Character Search'
-                info['url'] = url
-                info['description'] = ign
                 info['thumbnail'] = thumbnail
 
                 # Setting up additional field
 
-                extra = OrderedDict(
+                extra = \
                     [
-                        ('Server', {'value': server,
-                                    'inline': True}),
-                        ('Rank', {'value': ranking,
+                        ('Name', {'value': ign,
                                   'inline': True}),
+
                         ('Job', {'value': job,
                                  'inline': True}),
                         ('Level', {'value': level_string,
-                                   'inline': True})
+                                   'inline': True}),
+
+                        ('World', {'value': server,
+                                   'inline': True}),
+
+                        ('Overall Rank', {'value': ranking,
+                                          'inline': True}),
 
                     ]
-                )
-
-                info['extra'] = extra
-
+                found = True
                 break
+
+        if found:  # Character on ranking
+
+            # Find World Ranking
+            url = "http://maplestory.nexon.net/rankings/world-ranking/"+server+"?" \
+                        "pageIndex=1&character_name="+player_name+"&search=true#ranking"
+
+            source = requests.get(url)
+
+            soup = bs4.BeautifulSoup(source.text, 'html.parser')
+
+            for tr in soup.find_all('tr'):
+                if player_name.lower() in tr.text.lower():
+                    td = tr.find_all('td')
+                    extra.append(
+                        ('World Rank', {'value': str(td[0].string).strip(),
+                         'inline': True})
+                    )
+                    break
+
+            # Find Job Ranking
+
+            classification = None
+            job_filtered = job.strip().replace(' ', '-')
+
+            if job in self.explorer:
+                classification = 'explorer'
+            elif job in self.cygnus:
+                classification = 'cygnus-knights'
+            elif job in self.resistance:
+                classification = 'resistance'
+            elif job in self.sengoku:
+                classification = 'sengoku'
+            else:
+                classification = job_filtered
+
+            url = "http://maplestory.nexon.net/rankings/job-ranking/"+classification+"/"+job_filtered+"?" \
+                  "pageIndex=1&character_name="+player_name+"&search=true#ranking"
+
+            source = requests.get(url)
+
+            soup = bs4.BeautifulSoup(source.text, 'html.parser')
+
+            for tr in soup.find_all('tr'):
+                if player_name.lower() in tr.text.lower():
+                    td = tr.find_all('td')
+                    extra.append(
+                        ('Job Rank', {'value': str(td[0].string).strip(),
+                                      'inline': True})
+                    )
+                    break
+
+            # Find Legion Ranking
+
+            url = "http://maplestory.nexon.net/rankings/legion/"+server+"?pageIndex=1&character_name="+player_name+"&" \
+                                                                        "search=true#ranking"
+
+            source = requests.get(url)
+
+            soup = bs4.BeautifulSoup(source.text, 'html.parser')
+
+            for tr in soup.find_all('tr'):
+                if player_name.lower() in tr.text.lower():
+                    td = tr.find_all('td')
+                    rank_value = None
+                    try:
+                        l_rank = td[0].find('img')['src']
+                        rank_value = l_rank[-5]
+                    except Exception:
+                        pass
+                        rank_value = str(td[0].string).strip()
+
+                    extra.append(
+                        ('Legion Rank', {'value': rank_value,
+                         'inline': True})
+                    )
+                    break
+                else:
+                    extra.append(
+                        ('Legion Rank', {'value': 'N/A',
+                         'inline': True})
+                    )
+
+            info['extra'] = OrderedDict(extra)
 
         return info
